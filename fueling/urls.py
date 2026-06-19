dfc4cc8a445de5267a8f670f7bef1e5df08338aa
Badam <sohailmushtaqraja@gmail.com>
2026-06-19 15:18:46 +0500

    Fix: URLs so you don't encounter 404 error

--- fueling/urls.py
+++ fueling/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-
+from django.views.generic import TemplateView
 from fueling.views import MapView, RouteOptimizeView
 
 urlpatterns = [
+    path("", RedirectView.as_view(url="/map/", permanent=False)),
     path("api/route/", RouteOptimizeView.as_view(), name="route-optimize"),
     path("map/", MapView.as_view(), name="map"),
 ]
