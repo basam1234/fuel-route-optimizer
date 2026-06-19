37dd47502e2fad0e654f72c65b67c5b661712470
Badam <sohailmushtaqraja@gmail.com>
2026-06-19 15:29:07 +0500

    Fix: URLs for fueling

--- fueling/urls.py
+++ fueling/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.views.generic import TemplateView
+from django.views.generic import RedirectView
 from fueling.views import MapView, RouteOptimizeView
 
 urlpatterns = [
